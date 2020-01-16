d561cd76802403713a20325e27026dc0796bdf81
Fran Bosquet <frbosquet@gmail.com>
2020-01-16 18:56:01 +0100

    Add helpers to get available numbers in a rows, column and blocks

--- Assets/Scripts/SlabManager.cs
+++ Assets/Scripts/SlabManager.cs
@@ -21,7 +21,6 @@ public class SlabManager : MonoBehaviour
         slab.SetNumber(indexes[i, j]);
       }
     }
-
   }
 
   void PopulateIndexes()
@@ -33,5 +32,68 @@ public class SlabManager : MonoBehaviour
         indexes[i, j] = j + 1;
       }
     }
+  }
+
+  int[] IndexToPosition(int index)
+  {
+    int j = index % 9;
+    int i = Mathf.FloorToInt(index / 9);
+
+    return new int[] { i, j };
+  }
+
+  int PositionToIndex(int i, int j)
+  {
+    return i * 9 + j;
+  }
+
+  List<int> AvailableInRow(int row)
+  {
+    List<int> availables = ValidValues();
+
+    for (int j = 0; j < 9; j++)
+    {
+      availables.Remove(indexes[row, j]);
+    }
+
+    return availables;
+  }
+
+  List<int> AvailableInColumn(int column)
+  {
+    List<int> availables = ValidValues();
+
+    for (int i = 0; i < 9; i++)
+    {
+      availables.Remove(indexes[i, column]);
+    }
+
+    return availables;
+  }
+
+
+  List<int> AvailableInBlock(int row, int column)
+  {
+    List<int> availables = ValidValues();
+
+    int mini = Mathf.FloorToInt(row / 3);
+    int minj = Mathf.FloorToInt(column / 3);
+
+    for (int i = mini; i < mini + 3; i++)
+    {
+      for (int j = minj; j < minj + 3; j++)
+      {
+        availables.Remove(indexes[i, j]);
+      }
+    }
+
+    return availables;
+  }
+
+  List<int> ValidValues()
+  {
+    return new List<int>{
+        1,2,3,4,5,6,7,8,9
+    };
   }
 }
